0b3fb92518e28289cbbe0606b80b017683fe514b
Divij Vaidya <divijvaidya13@gmail.com>
2011-12-21 15:56:28 +0530

    framework expanded

--- blog.py
+++ blog.py
@@ -18,12 +18,20 @@ class Application(tornado.web.Application):
 	def __init__(self):
 		handlers=[
 			(r"/",HomeHandler),
-	#		(r"/auth/login",AuthHandler),
-	#		(r"/auth/logout",AuthHandler),
-			(r"/compose",ComposeHandler)
+			(r"/auth/login",AuthLoginHandler),
+			(r"/auth/logout",AuthLogoutHandler),
+			(r"/compose",ComposeHandler),
+			(r"/about",AboutHandler),
+			(r"/feed",FeedHandler)
 		]
 
-		tornado.web.Application.__init__(self,handlers)
+		setting=dict(
+			xsrf_cookies=True,
+			login_url="auth/login",
+			cookie_secret="AsdfsvAFDFavdaSVvfaA214eQEd324w2dF",
+		)
+
+		tornado.web.Application.__init__(self,handlers,**setting)
 
 		connect('blog_database')
 
@@ -39,10 +47,12 @@ class Comment(EmbeddedDocument):
         name = StringField(max_length=120)
 
 class Post(Document):
+	postid=IntField()
         title = StringField(max_length=120, required=True)
         author = ReferenceField(User, reverse_delete_rule=CASCADE)
         tags = ListField(StringField(max_length=30))
         comments = ListField(EmbeddedDocumentField(Comment))
+	date=DateTimeField()
 
 class TextPost(Post):
         content = StringField()
@@ -56,7 +66,6 @@ class LinkPost(Post):
 class BaseHandler(tornado.web.RequestHandler):
 	def get_current_user(self):
 		#user_id=self.get_secure_cookie("user")
-		self.write("Returning user ID 1000")
 		user_id=1000
 		if not user_id: return None
 		return User.objects(usrid=user_id)
@@ -66,7 +75,7 @@ class HomeHandler(BaseHandler):
 		self.write('Inside HomeHandler')
 		usr=self.get_current_user()
 		if len(usr)>1:
-			self.write("More than two people logged in ")
+			self.write("More than one person logged in ")
 			return None
 		
 		for authobj in usr:
@@ -78,7 +87,27 @@ class HomeHandler(BaseHandler):
 
 class ComposeHandler(BaseHandler):
 	def get(self):
-		self.write("You have reached compose page")
+		pstid=self.get_argument("id",None);
+		entry=None;
+		if pstid:
+			#if a post already exists by the same ID
+			entry=Post.objects(postid=pstid)
+		self.render("compose.html",entry=entry)
+	
+class AboutHandler(BaseHandler):
+	def get(self):
+		self.render("about.html")
+
+class FeedHandler(BaseHandler):
+	def get(self):
+		self.write("You have reached feed page")
+class AuthLoginHandler():
+	def get(self):
+		self.write("You have reached login page")
+class AuthLogoutHandler():
+	def get(self):
+		self.write("You have rached logout page")
+		
 def main():
 	http_server = tornado.httpserver.HTTPServer(Application())
 	http_server.listen(options.port)
